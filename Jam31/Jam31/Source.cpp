ac74005d5aac6afd4aa0656b46864c13903de9a1
Dr. J. Moriarty <Jennifer_Moriarty@dpsk12.org>
2017-01-31 08:22:50 -0700

    insult
    I added a mean comment.

--- Jam31/Jam31/Source.cpp
+++ Jam31/Jam31/Source.cpp
@@ -4,4 +4,4 @@ int main() {
 	cout << "I wanted to eat a clock but I realized" << endl;
 	cout << "..................." << endl;
 	cout << "It was too time consuming" << endl;
-}
+}//yuck. bad joke.
