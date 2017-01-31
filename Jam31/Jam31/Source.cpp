4466d94d148da9e68a1513d2bde05744e130cc12
Game110AM <Game110AM@605-110-STU11>
2017-01-31 07:43:45 -0700

    t

---
+++ Jam31/Jam31/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "I wanted to eat a clock but I realized" << endl;
+	cout << "..................." << endl;
+	cout << "It was too time consuming" << endl;
+}
