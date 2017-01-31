a8a3f60ff8886158ebe97f0a288aa7b1b55bddcc
Game110AM <Game110AM@605-110-STU11>
2017-01-31 12:59:58 -0700

    FunnyJoke

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "What do you call a nun who just passed her bar exam?" << endl;
+    cout << "A sister-in-law." << endl;
+}
+  
---
+++ jam25/jam25/Source.cpp
@@ -0,0 +1,6 @@
+#include < iostream>
+using namespace std;
+int main() {
+const char firstinitial = "p";
+	cout << "Welcome," << firstinitial << "!!" << endl;
+}
